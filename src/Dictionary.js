1f3d08ed81cc0e5bb9a5e33dd855f7a2f10e14b6
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-02-29 16:27:24 +0000

    Added error handling for incorrect words

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,24 +6,32 @@ import Results from "./Results";
 export default function Dictionary() {
     let [searchTerm, setSearchTerm] = useState("");
     let [resultData, setResultData] = useState(null);
+    let [error, setError] = useState(false);
     
 
         function handleResponse(response) {
         
          setResultData(response.data);
+         setError(false);
         
     }
-
+function handleError() {
+    setError(true); 
+    setResultData(null); 
+}
      function search(event) {
         event.preventDefault(); 
+        setError(false);
 
         if (searchTerm !== "") { 
             let apiKey = "ab6f607t41943e02220ae3724eo64aeb";
             let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${searchTerm}&key=${apiKey}`;
 
-            axios.get(apiUrl).then(handleResponse);
+            axios.get(apiUrl).then(handleResponse).catch(handleError);
         }
     }
+
+    
 
     function handleSearchTermChange(event) {
               setSearchTerm(event.target.value);
@@ -37,7 +45,7 @@ export default function Dictionary() {
             <input type="search" placeholder="Search for a word..." autoFocus="on" onChange={handleSearchTermChange}/>
             <input type="submit" value="Search" />
         </form>
-        <Results data={resultData}/>
+        <Results data={resultData} error={error}/>
     </div>
 );
 }
--- src/Results.js
+++ src/Results.js
@@ -1,17 +1,21 @@
 import React from "react";
 import Meanings from "./Meanings";
 
-export default function Results(props){
-     if (props.data) {
-    return (
-      <div className="Results">
-        <h2>{props.data.word}</h2>
-        {props.data.meanings.slice(0, 3).map((meaning, index) => (
-          <Meanings key={index} meaning={meaning} />
-        ))}
-      </div>
-    );
-  } else {
-    return null;
-  }
+export default function Results(props) {
+    if (props.data && props.data.meanings) {
+        return (
+            <div className="Results">
+                <h2>{props.data.word}</h2>
+                {props.data.meanings.slice(0, 3).map((meaning, index) => (
+                    <Meanings key={index} meaning={meaning} />
+                ))}
+            </div>
+        );
+    } else if (props.error) {
+        
+        return <div className="Error">Sorry, we couldn't find the word you were looking for. Please try a different search.</div>;
+    } else {
+        
+        return null;
+    }
 }
